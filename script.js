cbfb4c4777f2876b38c6dd1b2697bd28c1b11b10
LourencoFreire <lourencofreire3@gmail.com>
2026-04-01 12:02:40 +0100

    Added time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const background = document.getElementById("background");
+const timeEl = document.getElementById("time");
 
 async function getBackground() {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
@@ -7,3 +8,13 @@ async function getBackground() {
 }
 
 getBackground();
+
+setInterval(() => {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    timeEl.innerText = hours + ":" + minutes + ":" + seconds;
+}, 1000);
